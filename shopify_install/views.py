fbfd6e92a45c11658cb91a0504afd2f40c8c69cc
Igor Sobreira <igor@igorsobreira.com>
2017-05-02 19:16:53 -1000

    app redirect url

--- shopify_install/views.py
+++ shopify_install/views.py
@@ -10,16 +10,21 @@ SHOPIFY_APP_URL = settings.SHOPIFY_APP_URL
 SHOPIFY_API_KEY = settings.SHOPIFY_API_KEY
 SHOPIFY_API_SECRET = settings.SHOPIFY_API_SECRET
 
+# this value must be the same set in your application config
+# in shopify
+APP_REDIRECT_PATH = '/auth/callback/'
+
 def install(request):
     """
     Link merchanges will be sent to to install this App in their store
     """
     shop = request.GET['shop']
     scopes = "read_customers,read_products,read_orders"
-    install_url = "http://%(shop)s/admin/oauth/authorize?client_id=%(key)s&scope=%(scopes)s&redirect_uri=%(app_url)s/shopify_install/callback/" % {
+    install_url = "http://%(shop)s/admin/oauth/authorize?client_id=%(key)s&scope=%(scopes)s&redirect_uri=%(app_url)s%(app_redirect_path)s" % {
         'shop': shop,
         'key': SHOPIFY_API_KEY,
         'app_url': SHOPIFY_APP_URL,
+        'app_redirect_path': APP_REDIRECT_PATH,
         'scopes': scopes,
     }
     return redirect(install_url)
