a6b59acb7b917c03fab86152a3754afd06fa8b50
Igor Sobreira <igor@igorsobreira.com>
2017-05-02 19:14:31 -1000

    fix syntax error on setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     description='Django App to install Shopify App',
     author='Igor Sobreira',
-    author_email='igor@igorsobreira.com'
+    author_email='igor@igorsobreira.com',
     url='https://github.com/igorsobreira/shopify_install',
     packages=['shopify_install', 'shopify_install.migrations'],
 )
